1937acbff360886a22213cd6829eaae09db0d4f9
hatada <koki.hatada@gmail.com>
2018-03-07 16:55:56 +0900

    tslint

--- src/convert.ts
+++ src/convert.ts
@@ -14,37 +14,36 @@ function parseValue(value: string): string | number{
 }
 
 function csvToJSON(input: string, output: string): void{
-    fs.readFile(input, (error, buffer)=>{
+    fs.readFile(input, (error, buffer) => {
         if(error){
             console.error("Cannot read file");
             console.error(error);
         }else{
-            const encoding = chardet.detect(buffer);
             const text = iconv.decode(buffer, "SHIFT_JIS");
-            csv.parse(text, (error, data: string[][])=>{
+            csv.parse(text, (error, data: string[][]) => {
                 if(error){
                     console.error("Cannot parse CSV");
                     console.error(error);
                 }else{
                     const header = data[0];
                     const json: any[] = [];
-                    for(let i=1; i<data.length; i++){
+                    for(let i = 1; i < data.length; i++){
                         const row = data[i];
                         const element = {};
-                        header.forEach((key, index)=>{
+                        header.forEach((key, index) => {
                             element[key] = parseValue(row[index]);
                         });
                         json.push(element);
                     }
                     console.log(json);
-                    fs.writeFile(output, JSON.stringify(json, null, 4), (error)=>{
+                    fs.writeFile(output, JSON.stringify(json, null, 4), (error) => {
                         if(error){
                             console.error("Cannot write file");
                             console.error(error);
                         }else{
                             console.log("Successfully write to " + output);
                         }
-                    })
+                    });
                 }
             });
         }
@@ -52,7 +51,7 @@ function csvToJSON(input: string, output: string): void{
 }
 
 function jsonToCSV(input: string, output: string): void{
-    fs.readFile(input, (error, buffer)=>{
+    fs.readFile(input, (error, buffer) => {
         if(error){
             console.error("Cannot read file");
             console.error(error);
@@ -62,14 +61,14 @@ function jsonToCSV(input: string, output: string): void{
             try{
                 const json: any[] = JSON.parse(text);
                 const header: string[] = [];
-                json.forEach((element)=>{
+                json.forEach((element) => {
                     Object.keys(element).forEach((key) => {
                         if(header.indexOf(key) < 0){
                             header.push(key);
                         }
-                    })
+                    });
                 });
-                const list = json.map((element)=>{
+                const list = json.map((element) => {
                     const row = header.map((key) => {
                         return element[key];
                     });
@@ -82,7 +81,7 @@ function jsonToCSV(input: string, output: string): void{
                         console.error(error);
                     }else{
                         const buffer = iconv.encode(data.replace(/\n/g, "\r\n"), "SHIFT_JIS");
-                        fs.writeFile(output, buffer, (error)=>{
+                        fs.writeFile(output, buffer, (error) => {
                             if(error){
                                 console.error("Cannot write file");
                                 console.error(error);
@@ -106,7 +105,7 @@ const output = process.argv[3];
 if(input){
     const ext = path.extname(input);
     if(ext && ext.length){
-        const withoutExt =input.substring(0, input.length - ext.length);
+        const withoutExt = input.substring(0, input.length - ext.length);
         if(ext === ".json"){
             if(output){
                 jsonToCSV(input, output);
